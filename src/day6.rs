eb8b2535089addb3b4a9c6040a2049a078ebef11
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-06 07:43:06 -0500

    day 6

---
+++ src/day6.rs
@@ -0,0 +1,29 @@
+use std::collections::HashSet;
+
+#[aoc(day6, part1)]
+fn part1(input: &[u8]) -> usize {
+    'windows: for (index, window) in input.windows(4).enumerate() {
+        let mut items = HashSet::with_capacity(4);
+        for item in window {
+            if !items.insert(item) {
+                continue 'windows;
+            }
+        }
+        return index + 4;
+    }
+    panic!("4 consecutive unique characters not found in input")
+}
+
+#[aoc(day6, part2)]
+fn part2(input: &[u8]) -> usize {
+    'windows: for (index, window) in input.windows(14).enumerate() {
+        let mut items = HashSet::with_capacity(14);
+        for item in window {
+            if !items.insert(item) {
+                continue 'windows;
+            }
+        }
+        return index + 14;
+    }
+    panic!("14 consecutive unique characters not found in input")
+}
