a128dcbad65adf0f799a5553503a335c8e265cf4
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-16 16:56:14 -0500

    day 16 p2

--- src/day16.rs
+++ src/day16.rs
@@ -1,8 +1,10 @@
+use itertools::Itertools;
 use std::{
     cell::{Cell, RefCell},
-    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    collections::{BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
     rc::Rc,
 };
+const THREADS: usize = 16;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Node {
@@ -210,6 +212,126 @@ fn part1(input: &HashMap<String, Rc<Node>>) -> usize {
     max_pressure
 }
 
+#[aoc(day16, part2)]
+fn part2(input: &HashMap<String, Rc<Node>>) -> usize {
+    let mut max_pressure = 0;
+    let mut queue = VecDeque::new();
+    let pressure_valves = input
+        .values()
+        .filter_map(|n| {
+            if n.pressure.get() > 0 {
+                Some(n.name.borrow().clone())
+            } else {
+                None
+            }
+        })
+        .collect::<BTreeSet<_>>();
+    for i in 0..pressure_valves.len() {
+        println!("{}", i);
+        let mut human_valve_iter = pressure_valves.iter().combinations(i);
+        while let Some(human_valves) = human_valve_iter.next() {
+            let human_valves = HashSet::from_iter(human_valves.into_iter().map(|s| s.clone()));
+            let mut human_max_pressure = 0;
+            let mut elephant_max_pressure = 0;
+
+            let elephant_valves = pressure_valves
+                .iter()
+                .map(|s| s.clone())
+                .filter(|n| !human_valves.contains(n))
+                .collect::<HashSet<_>>();
+
+            // println!("human valves: {:?}", human_valves);
+            // println!("elephant valves: {:?}", elephant_valves);
+
+            assert_eq!(
+                human_valves.len() + elephant_valves.len(),
+                pressure_valves.len()
+            );
+            assert_eq!(human_valves.intersection(&elephant_valves).count(), 0);
+
+            // run human
+            queue.push_back(State {
+                current: input.get(HEAD_NODE_NAME).unwrap().clone(),
+                pressure: 0,
+                remaining_time: 26,
+                closed_valves: human_valves.clone(),
+            });
+            while let Some(state) = queue.pop_front() {
+                human_max_pressure = std::cmp::max(human_max_pressure, state.pressure);
+                if state.remaining_time < 0 || state.closed_valves.len() == 0 {
+                    continue;
+                }
+                for dest in state.closed_valves.iter() {
+                    let dist_to_dest = *state
+                        .current
+                        .nonzero_node_distances
+                        .borrow()
+                        .get(dest)
+                        .unwrap() as isize;
+                    if dist_to_dest >= state.remaining_time {
+                        continue;
+                    }
+
+                    queue.push_back(State {
+                        current: input.get(dest).unwrap().clone(),
+                        remaining_time: state.remaining_time - dist_to_dest - 1,
+                        pressure: (state.pressure
+                            + (state.remaining_time - dist_to_dest - 1) as usize
+                                * input.get(dest).unwrap().pressure.get()),
+                        closed_valves: state
+                            .closed_valves
+                            .clone()
+                            .into_iter()
+                            .filter(|v| v != dest)
+                            .collect(),
+                    })
+                }
+            }
+            // run elephant
+            queue.push_back(State {
+                current: input.get(HEAD_NODE_NAME).unwrap().clone(),
+                pressure: 0,
+                remaining_time: 26,
+                closed_valves: elephant_valves.clone(),
+            });
+            while let Some(state) = queue.pop_front() {
+                elephant_max_pressure = std::cmp::max(elephant_max_pressure, state.pressure);
+                if state.remaining_time < 0 || state.closed_valves.len() == 0 {
+                    continue;
+                }
+                for dest in state.closed_valves.iter() {
+                    let dist_to_dest = *state
+                        .current
+                        .nonzero_node_distances
+                        .borrow()
+                        .get(dest)
+                        .unwrap() as isize;
+                    if dist_to_dest >= state.remaining_time {
+                        continue;
+                    }
+
+                    queue.push_back(State {
+                        current: input.get(dest).unwrap().clone(),
+                        remaining_time: state.remaining_time - dist_to_dest - 1,
+                        pressure: (state.pressure
+                            + (state.remaining_time - dist_to_dest - 1) as usize
+                                * input.get(dest).unwrap().pressure.get()),
+                        closed_valves: state
+                            .closed_valves
+                            .clone()
+                            .into_iter()
+                            .filter(|v| v != dest)
+                            .collect(),
+                    })
+                }
+            }
+            // println!("{}, {}", human_max_pressure, elephant_max_pressure);
+            max_pressure = std::cmp::max(max_pressure, human_max_pressure + elephant_max_pressure);
+        }
+    }
+    max_pressure
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -217,7 +339,12 @@ mod test {
     #[test]
     fn part1_example() {
         let input = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\nValve BB has flow rate=13; tunnels lead to valves CC, AA\nValve CC has flow rate=2; tunnels lead to valves DD, BB\nValve DD has flow rate=20; tunnels lead to valves CC, AA, EE\nValve EE has flow rate=3; tunnels lead to valves FF, DD\nValve FF has flow rate=0; tunnels lead to valves EE, GG\nValve GG has flow rate=0; tunnels lead to valves FF, HH\nValve HH has flow rate=22; tunnel leads to valve GG\nValve II has flow rate=0; tunnels lead to valves AA, JJ\nValve JJ has flow rate=21; tunnel leads to valve II";
-
         assert_eq!(part1(&(input_generator(input))), 1651);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\nValve BB has flow rate=13; tunnels lead to valves CC, AA\nValve CC has flow rate=2; tunnels lead to valves DD, BB\nValve DD has flow rate=20; tunnels lead to valves CC, AA, EE\nValve EE has flow rate=3; tunnels lead to valves FF, DD\nValve FF has flow rate=0; tunnels lead to valves EE, GG\nValve GG has flow rate=0; tunnels lead to valves FF, HH\nValve HH has flow rate=22; tunnel leads to valve GG\nValve II has flow rate=0; tunnels lead to valves AA, JJ\nValve JJ has flow rate=21; tunnel leads to valve II";
+        assert_eq!(part2(&(input_generator(input))), 1707);
     }
 }
