877c8c2f9ed09d3a857c0d14e42f5a497ad601f2
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-18 14:28:53 -0500

    day 18 p2

--- src/day18.rs
+++ src/day18.rs
@@ -44,6 +44,100 @@ fn part1(input: &HashSet<(isize, isize, isize)>) -> isize {
     surface_area
 }
 
+#[aoc(day18, part2)]
+fn part2(input: &HashSet<(isize, isize, isize)>) -> isize {
+    let (min_x, min_y, min_z) = input.iter().fold(
+        (isize::MAX, isize::MAX, isize::MAX),
+        |(x1, y1, z1), (x2, y2, z2)| {
+            (
+                std::cmp::min(x1, *x2),
+                std::cmp::min(y1, *y2),
+                std::cmp::min(z1, *z2),
+            )
+        },
+    );
+
+    let (min_x, min_y, min_z) = (min_x - 1, min_y - 1, min_z - 1);
+
+    let (max_x, max_y, max_z) = input.iter().fold(
+        (isize::MIN, isize::MIN, isize::MIN),
+        |(x1, y1, z1), (x2, y2, z2)| {
+            (
+                std::cmp::max(x1, *x2),
+                std::cmp::max(y1, *y2),
+                std::cmp::max(z1, *z2),
+            )
+        },
+    );
+    let (max_x, max_y, max_z) = (max_x + 1, max_y + 1, max_z + 1);
+
+    let mut external_squares = HashSet::new();
+    let mut flood_queue = vec![(min_x, min_y, min_z)];
+    while let Some((x, y, z)) = flood_queue.pop() {
+        external_squares.insert((x, y, z));
+        if !input.contains(&(x - 1, y, z))
+            && !external_squares.contains(&(x - 1, y, z))
+            && x - 1 >= min_x
+        {
+            flood_queue.push((x - 1, y, z));
+        }
+        if !input.contains(&(x + 1, y, z))
+            && !external_squares.contains(&(x + 1, y, z))
+            && x + 1 <= max_x
+        {
+            flood_queue.push((x + 1, y, z));
+        }
+        if !input.contains(&(x, y - 1, z))
+            && !external_squares.contains(&(x, y - 1, z))
+            && y - 1 >= min_y
+        {
+            flood_queue.push((x, y - 1, z));
+        }
+        if !input.contains(&(x, y + 1, z))
+            && !external_squares.contains(&(x, y + 1, z))
+            && y + 1 <= max_y
+        {
+            flood_queue.push((x, y + 1, z));
+        }
+        if !input.contains(&(x, y, z - 1))
+            && !external_squares.contains(&(x, y, z - 1))
+            && z - 1 >= min_z
+        {
+            flood_queue.push((x, y, z - 1));
+        }
+        if !input.contains(&(x, y, z + 1))
+            && !external_squares.contains(&(x, y, z + 1))
+            && z + 1 <= max_z
+        {
+            flood_queue.push((x, y, z + 1));
+        }
+    }
+
+    let mut surface_area = 0;
+
+    for (x, y, z) in external_squares.into_iter() {
+        if input.contains(&(x - 1, y, z)) {
+            surface_area += 1
+        }
+        if input.contains(&(x + 1, y, z)) {
+            surface_area += 1
+        }
+        if input.contains(&(x, y - 1, z)) {
+            surface_area += 1
+        }
+        if input.contains(&(x, y + 1, z)) {
+            surface_area += 1
+        }
+        if input.contains(&(x, y, z - 1)) {
+            surface_area += 1
+        }
+        if input.contains(&(x, y, z + 1)) {
+            surface_area += 1
+        }
+    }
+    surface_area
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -54,5 +148,10 @@ mod test {
     #[test]
     fn part1_example() {
         assert_eq!(part1(&input_generator(INPUT)), 64);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&input_generator(INPUT)), 58);
     }
 }
