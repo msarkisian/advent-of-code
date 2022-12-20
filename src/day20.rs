9e9dbff5d5bf8a03221a774d0e5eaa44529a2a2e
Michael Sarkisian <msarkisian1996@gmail.com>
2022-12-20 18:10:10 -0500

    day 20 p2

--- src/day20.rs
+++ src/day20.rs
@@ -16,13 +16,18 @@ fn part1(input: &str) -> isize {
         while !Rc::ptr_eq(cursor.current().unwrap(), item) {
             cursor.move_next();
         }
+        if *item.as_ref() == 0 {
+            continue;
+        }
 
-        // let steps_to_new = item.as_ref() % (list_len as isize - 1);
-        let steps_to_new = *item.as_ref();
+        let steps_to_new = item.as_ref() % (list_len as isize - 1);
 
         let item = cursor.remove_current().unwrap();
         if steps_to_new > 0 {
             for _ in 0..steps_to_new {
+                if cursor.current() == None {
+                    cursor.move_next()
+                }
                 cursor.move_next();
                 if cursor.current() == None {
                     cursor.move_next()
@@ -40,7 +45,6 @@ fn part1(input: &str) -> isize {
             cursor.move_prev()
         }
         cursor.insert_before(item);
-        // println!("{:?}", list);
     }
     let mut cursor = list.cursor_front();
     let mut zero_index = 0;
@@ -48,8 +52,6 @@ fn part1(input: &str) -> isize {
         cursor.move_next();
         zero_index += 1;
     }
-
-    println!("{:?}", list);
 
     let first;
     let mut cursor = list.cursor_front();
@@ -73,7 +75,86 @@ fn part1(input: &str) -> isize {
     }
     third = cursor.current().unwrap().as_ref();
 
-    println!("{}, {}, {}", first, second, third);
+    first + second + third
+}
+
+#[aoc(day20, part2)]
+fn part2(input: &str) -> isize {
+    const DECRYPTION_KEY: isize = 811589153;
+    let mut list = LinkedList::new();
+    for line in input.lines() {
+        list.push_back(Rc::new(line.parse::<isize>().unwrap() * DECRYPTION_KEY))
+    }
+    let list_len = list.len();
+    let mut original_order = Vec::with_capacity(list.len());
+    for rc in list.iter() {
+        original_order.push(rc.clone());
+    }
+
+    for item in std::iter::repeat(original_order.iter()).take(10).flatten() {
+        let mut cursor = list.cursor_front_mut();
+        while !Rc::ptr_eq(cursor.current().unwrap(), item) {
+            cursor.move_next();
+        }
+
+        if *item.as_ref() == 0 {
+            continue;
+        }
+
+        let steps_to_new = item.as_ref() % (list_len as isize - 1);
+
+        let item = cursor.remove_current().unwrap();
+        if steps_to_new > 0 {
+            for _ in 0..steps_to_new {
+                if cursor.current() == None {
+                    cursor.move_next();
+                }
+                cursor.move_next();
+                if cursor.current() == None {
+                    cursor.move_next();
+                }
+            }
+        } else if steps_to_new < 0 {
+            for _ in 0..steps_to_new.abs() {
+                cursor.move_prev();
+                if cursor.current() == None {
+                    cursor.move_prev()
+                }
+            }
+        }
+        if cursor.peek_prev() == None {
+            cursor.move_prev()
+        }
+        cursor.insert_before(item);
+    }
+    let mut cursor = list.cursor_front();
+    let mut zero_index = 0;
+    while cursor.current().unwrap().as_ref() != &0 {
+        cursor.move_next();
+        zero_index += 1;
+    }
+
+    let first;
+    let mut cursor = list.cursor_front();
+    let first_steps = (1000 + zero_index) % list_len;
+    for _ in 0..first_steps {
+        cursor.move_next();
+    }
+    first = cursor.current().unwrap().as_ref();
+    let second;
+    let mut cursor = list.cursor_front();
+    let second_steps = (2000 + zero_index) % list_len;
+    for _ in 0..second_steps {
+        cursor.move_next();
+    }
+    second = cursor.current().unwrap().as_ref();
+    let third;
+    let mut cursor = list.cursor_front();
+    let third_steps = (3000 + zero_index) % list_len;
+    for _ in 0..third_steps {
+        cursor.move_next();
+    }
+    third = cursor.current().unwrap().as_ref();
 
     first + second + third
 }
@@ -86,5 +167,10 @@ mod test {
     #[test]
     fn part1_example() {
         assert_eq!(part1(INPUT), 3)
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(INPUT), 1623178306)
     }
 }
