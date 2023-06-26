c247eae021d92ee839a2472340958f6a8fd569eb
Michael Sarkisian <msarkisian1996@gmail.com>
2023-06-26 15:46:09 -0400

    2016 d1 p1

---
+++ 2016/1.py
@@ -0,0 +1,71 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    NORTH = 1
+    EAST = 2
+    SOUTH = 3
+    WEST = 4
+
+    def left(self):
+        match self:
+            case Direction.NORTH:
+                return Direction.WEST
+            case Direction.WEST:
+                return Direction.SOUTH
+            case Direction.SOUTH:
+                return Direction.EAST
+            case Direction.EAST:
+                return Direction.NORTH
+
+    def right(self):
+        match self:
+            case Direction.NORTH:
+                return Direction.EAST
+            case Direction.WEST:
+                return Direction.NORTH
+            case Direction.SOUTH:
+                return Direction.WEST
+            case Direction.EAST:
+                return Direction.SOUTH
+
+
+def part1(directions: list[str]):
+    facing = Direction.NORTH
+    x, y = 0, 0
+    for dir in directions:
+        match dir[0]:
+            case 'R':
+                facing = facing.right()
+            case 'L':
+                facing = facing.left()
+            case other:
+                raise Exception("invalid direction in input")
+        steps = int(dir[1:])
+        match facing:
+            case Direction.NORTH:
+                y += steps
+            case Direction.SOUTH:
+                y -= steps
+            case Direction.EAST:
+                x += steps
+            case Direction.WEST:
+                x -= steps
+    return abs(x) + abs(y)
+
+
+# TEST
+# directions = ["R2", "L3"]
+# print(part1(directions))  # 5
+
+# directions = ["R2", "R2", "R2"]
+# print(part1(directions))  # 2
+
+# directions = ["R5", "L5", "R5", "R3"]
+# print(part1(directions))  # 12
+
+if __name__ == "__main__":
+    with open("./input/1.txt") as file:
+        directions = file.read().split(", ")
+        print(directions)
+        print(part1(directions))
