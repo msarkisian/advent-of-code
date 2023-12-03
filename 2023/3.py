a662f875535712b91ccf7a44895517b76478b322
Michael Sarkisian <msarkisian1996@gmail.com>
2023-12-03 11:45:40 -0500

    day 3 p2

--- 2023/3.py
+++ 2023/3.py
@@ -85,14 +85,100 @@ def part_1(input: str) -> int:
     return sum
 
 
+def part_2(input: str) -> int:
+    gear_locations = set()
+    number_locations: dict[(int, int), GridNumber] = {}
+    num_buffer = ""
+    loc_buffer = []
+    for y, line in enumerate(input.splitlines()):
+        for x, char in enumerate(line):
+            if char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                num_buffer += char
+                loc_buffer.append((x, y))
+            elif num_buffer:
+                grid_number = GridNumber(False, int(num_buffer))
+                for loc in loc_buffer:
+                    number_locations[loc] = grid_number
+                num_buffer = ""
+                loc_buffer = []
+            if char == '*':
+                gear_locations.add((x, y))
+        if num_buffer:
+            grid_number = GridNumber(False, int(num_buffer))
+            for loc in loc_buffer:
+                number_locations[loc] = grid_number
+            num_buffer = ""
+            loc_buffer = []
+    total = 0
+    for (x, y) in gear_locations:
+        found_nums: list[GridNumber] = []
+        # up left
+        if (x - 1, y - 1) in number_locations:
+            num = number_locations[(x-1, y-1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # up
+        if (x, y - 1) in number_locations:
+            num = number_locations[(x, y - 1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # up right
+        if (x + 1, y - 1) in number_locations:
+            num = number_locations[(x + 1, y - 1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # left
+        if (x - 1, y) in number_locations:
+            num = number_locations[(x - 1, y)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # right
+        if (x + 1, y) in number_locations:
+            num = number_locations[(x + 1, y)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # down left
+        if (x - 1, y + 1) in number_locations:
+            num = number_locations[(x - 1, y + 1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # down
+        if (x, y + 1) in number_locations:
+            num = number_locations[(x, y + 1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        # down right
+        if (x + 1, y + 1) in number_locations:
+            num = number_locations[(x + 1, y + 1)]
+            if not num.processed:
+                found_nums.append(num)
+                num.processed = True
+        if len(found_nums) == 2:
+            total += found_nums[0].val * found_nums[1].val
+        for fn in found_nums:
+            fn.processed = False
+    return total
+
+
 class Test(unittest.TestCase):
     def test_part_1(self):
         with open("./input/3_test.txt") as file:
             self.assertEqual(part_1(file.read()), 4361)
+
+    def test_part_2(self):
+        with open("./input/3_test.txt") as file:
+            self.assertEqual(part_2(file.read()), 467835)
 
 
 if __name__ == "__main__":
     with open("./input/3.txt") as file:
         input = file.read()
         print(f"Part 1: {part_1(input)}")
-        # print(f"Part 2: {part_2(input)}")
+        print(f"Part 2: {part_2(input)}")
