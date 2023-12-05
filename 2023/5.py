3d172035ad4afba49a6c9c76a5ebf36bc2154ae0
Michael Sarkisian <msarkisian1996@gmail.com>
2023-12-05 11:31:27 -0500

    day 5 p2 wip

--- 2023/5.py
+++ 2023/5.py
@@ -5,13 +5,56 @@ import dataclasses
 @dataclasses.dataclass
 class Range():
     start: int
+    end: int
+
+    @property
+    def length(self):
+        return self.end - self.start + 1
+
+
+@dataclasses.dataclass
+class RangeMap():
+    start: int
     length: int
     offset: int
+
+    @property
+    def end(self):
+        return self.start + self.length - 1
 
     def map_val(self, val: int) -> int | None:
         if self.start <= val < self.start + self.length:
             return val + self.offset
         return None
+
+
+def map_ranges(ranges: list[Range], range_maps: list[RangeMap]) -> list[Range]:
+    queue = ranges
+    output = []
+    for r in queue:
+        for rm in range_maps:
+            if r.start >= rm.start and r.end <= rm.end:
+                # rm perfectly contains range
+                output.append(Range(r.start + rm.offset, r.end + rm.offset))
+                break
+            elif r.start >= rm.start and r.end > rm.end:
+                # range overflows rm end
+                output.append(Range(r.start + rm.offset, rm.end + rm.offset))
+                queue.append(Range(rm.end + 1, r.end))
+                break
+            elif r.start < rm.start and r.end <= rm.end:
+                # range overflows rm start
+                output.append(Range(rm.start + rm.offset, r.end + rm.offset))
+                queue.append(Range(r.start, r.end - 1))
+                break
+            elif r.start < rm.start and r.end > rm.end:
+                # range overflows on both ends
+                output.append(Range(rm.start + rm.offset, rm.end + rm.offset))
+                queue.append(r.start, rm.start - 1)
+                queue.append(rm.end + 1, r.end)
+        # no matches found on any map
+        output.append(r)
+    return output
 
 
 def part_1(input: str) -> int:
@@ -36,7 +79,7 @@ def part_1(input: str) -> int:
             map_idx += 2
             continue
         vals = maps[map_idx].split(' ')
-        ranges.append(Range(int(vals[1]), int(
+        ranges.append(RangeMap(int(vals[1]), int(
             vals[2]), int(vals[0]) - int(vals[1])))
         map_idx += 1
 
@@ -53,6 +96,19 @@ def part_1(input: str) -> int:
     return min(locations)
 
 
+def part_2(input: str) -> int:
+    lines = input.splitlines()
+    seeds, maps = lines[0].split(' ')[1:], lines[2:]
+    seed_idx = 0
+    ranges = []
+    while seed_idx < len(seeds):
+        start = int(seeds[seed_idx])
+        length = int(seeds[seed_idx + 1])
+        ranges.append(Range(start, start + length - 1))
+        seed_idx += 2
+    print(ranges)
+
+
 class Test(unittest.TestCase):
   # Expected transformations:
   # [79, 14, 55, 13]
@@ -66,6 +122,10 @@ class Test(unittest.TestCase):
     def test_part_1(self):
         with open("../input/2023/5_test.txt") as f:
             self.assertEqual(part_1(f.read()), 35)
+
+    def test_part_2(self):
+        with open("../input/2023/5_test.txt") as f:
+            self.assertEqual(part_2(f.read()), 46)
 
 
 if __name__ == "__main__":
