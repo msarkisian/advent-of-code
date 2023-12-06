30b99c2290699e8ed02068fd62f55d7f1cb72bc3
Michael Sarkisian <msarkisian1996@gmail.com>
2023-12-06 09:28:34 -0500

    day 6 p2

--- 2023/6.py
+++ 2023/6.py
@@ -28,14 +28,37 @@ def part_1(input: str) -> int:
     return math.prod(ways)
 
 
+def part_2(input: str) -> int:
+    lines = input.splitlines()
+    time = int(re.sub(r'\s+', '', lines[0].strip("Time: ")))
+    distance = int(re.sub(r'\s+', '', lines[1].strip("Distance: ")))
+
+    best_time, mod = divmod(time, 2)
+    ways_found = 0
+    if (best_time * (time - best_time)) > distance:
+        if mod != 0:
+            ways_found += 2
+        else:
+            ways_found += 1
+    this_time = best_time - 1
+    while (this_time * (time - this_time)) > distance:
+        ways_found += 2
+        this_time -= 1
+    return ways_found
+
+
 class Test(unittest.TestCase):
     def test_part_1(self):
         with open("../input/2023/6_test.txt") as f:
             self.assertEqual(part_1(f.read()), 288)
+
+    def test_part_2(self):
+        with open("../input/2023/6_test.txt") as f:
+            self.assertEqual(part_2(f.read()), 71503)
 
 
 if __name__ == "__main__":
     with open("../input/2023/day6.txt") as f:
         input = f.read()
         print(f"Part 1: {part_1(input)}")
-        # print(f"Part 2: {part_2(input)}")
+        print(f"Part 2: {part_2(input)}")
